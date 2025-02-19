a451ad90570b39859bef435b5f0f65f83a52ec9e
illyanaAvelar <illyavelar012@gmail.com>
2025-02-19 19:54:46 -0300

    initial configuration

---
+++ server/src/server.ts
@@ -0,0 +1,41 @@
+import { fastifyCors } from "@fastify/cors";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
+import { fastify } from "fastify";
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import { subscribeToEventRoute } from "./routes/subscribe-to-event-route";
+import { env } from "./env";
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+
+app.register(fastifyCors, {
+  origin: true,
+});
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "NLW Connect",
+      version: "0.0.1",
+    },
+  },
+  transform: jsonSchemaTransform,
+});
+
+app.register(fastifySwaggerUi, {
+  routePrefix: "/docs",
+});
+
+app.register(subscribeToEventRoute);
+
+app.listen({ port: env.PORT }).then(() => {
+  console.log("HTTP server running");
+});
