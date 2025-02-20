421638fd864f02b8bf10db19e0aa26f449c81c1e
illyanaAvelar <illyavelar012@gmail.com>
2025-02-20 20:05:24 -0300

    initial routes and functions

---
+++ server/src/functions/subscribe-to-event.ts
@@ -0,0 +1,36 @@
+import { eq } from "drizzle-orm";
+import { db } from "../drizzle/client";
+import { subscriptions } from "../drizzle/schema/subscriptions";
+
+interface SubscribeToEventParams {
+	name: string;
+	email: string;
+}
+
+export async function subscribeToEvent({
+	name,
+	email,
+}: SubscribeToEventParams) {
+	const subscribers = await db
+		.select()
+		.from(subscriptions)
+		.where(eq(subscriptions.email, email));
+
+	if (subscribers.length > 0) {
+		return { subscriberId: subscribers[0].id };
+	}
+
+	const result = await db
+		.insert(subscriptions)
+		.values({
+			name,
+			email,
+		})
+		.returning();
+
+	const subscriber = result[0];
+
+	return {
+		subscriberId: subscriber.id,
+	};
+}
--- server/src/server.ts
+++ server/src/server.ts
@@ -3,13 +3,14 @@ import fastifySwagger from "@fastify/swagger";
 import fastifySwaggerUi from "@fastify/swagger-ui";
 import { fastify } from "fastify";
 import {
-  jsonSchemaTransform,
-  serializerCompiler,
-  validatorCompiler,
-  type ZodTypeProvider,
+	type ZodTypeProvider,
+	jsonSchemaTransform,
+	serializerCompiler,
+	validatorCompiler,
 } from "fastify-type-provider-zod";
-import { subscribeToEventRoute } from "./routes/subscribe-to-event-route";
 import { env } from "./env";
+import { accessInviteLinkRoute } from "./routes/access-invite-link";
+import { subscribeToEventRoute } from "./routes/subscribe-to-event-route";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -17,25 +18,26 @@ app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
 
 app.register(fastifyCors, {
-  origin: true,
+	origin: true,
 });
 
 app.register(fastifySwagger, {
-  openapi: {
-    info: {
-      title: "NLW Connect",
-      version: "0.0.1",
-    },
-  },
-  transform: jsonSchemaTransform,
+	openapi: {
+		info: {
+			title: "NLW Connect",
+			version: "0.0.1",
+		},
+	},
+	transform: jsonSchemaTransform,
 });
 
 app.register(fastifySwaggerUi, {
-  routePrefix: "/docs",
+	routePrefix: "/docs",
 });
 
 app.register(subscribeToEventRoute);
+app.register(accessInviteLinkRoute);
 
 app.listen({ port: env.PORT }).then(() => {
-  console.log("HTTP server running");
+	console.log("HTTP server running");
 });
